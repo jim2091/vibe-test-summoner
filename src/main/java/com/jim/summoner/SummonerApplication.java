16be642382a28e06a4303c168d8c349bff6fff23
jim2091 <jim2091@naver.com>
2026-09-23 21:48:24 +0900

    feat: initialize Spring Boot backend

---
+++ src/main/java/com/jim/summoner/SummonerApplication.java
@@ -0,0 +1,13 @@
+package com.jim.summoner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SummonerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SummonerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jim/summoner/SummonerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jim.summoner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SummonerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
